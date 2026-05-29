8b0cc2f1d2ea49e4295eeaa1eaa82e952f452e4d
ceciliademoraesa <cmao@cesar.school>
2026-05-29 00:09:55 -0300

    funçoes delete e excluir

---
+++ data/menu.py
@@ -0,0 +1,29 @@
+MENU_PRINCIPAL = """
+
+╔══════════════════════════════════╗
+║           ☀  ADOTA+  ☀          ║
+║   Sistema de adoção facilitada   ║
+╠══════════════════════════════════╣
+║  [1] - Cadastrar animal          ║
+║  [2] - Visualizar animal         ║
+║  [3] - Editar animal             ║
+║  [4] - Excluir animal            ║
+║  [5] - Sugestões personalizadas  ║
+║  [6] - *EXTRA                    ║
+║  [7] - Sair                      ║
+╚══════════════════════════════════╝
+
+Qual opção você deseja: """
+
+
+MENU_SAIDA = """
+[1] - Menu principal
+[2] - Encerrar o progama
+
+Qual opção você deseja: """
+
+MENU_RECUPERAR_ID = """
+[1] - Recuperar ID
+[2] - Sair 
+
+Qual opção você deseja: """
