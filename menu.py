96cd0c68fc61d7507256aa4e3a0c1394e8a3ad53
ceciliademoraesa <cmao@cesar.school>
2026-06-08 14:33:11 -0300

    feat:corrige menu

--- menu.py
+++ menu.py
@@ -19,14 +19,3 @@ MENU_PRINCIPAL = """
 Qual opção você deseja: """
 
 
-MENU_SAIDA = """
-[1] - Menu principal
-[2] - Encerrar o progama
-
-Qual opção você deseja: """
-
-MENU_RECUPERAR_ID = """
-[1] - Recuperar ID
-[2] - Sair 
-
-Qual opção você deseja: """
